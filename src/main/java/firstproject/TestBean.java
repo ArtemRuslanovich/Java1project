5fd23211cbd9a32078964b50f3f96c3b730238f7
Artiom Zavatski <zawatski2018@mail.ru>
2023-05-05 11:31:38 +0300

    Create first project

---
+++ src/main/java/firstproject/TestBean.java
@@ -0,0 +1,17 @@
+package firstproject;
+
+public class TestBean {
+    private String Name;
+
+    public TestBean(String name) {
+        this.Name = name;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        this.Name = name;
+    }
+}
